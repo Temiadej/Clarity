5be390f37944ff773bde95e3f3c72383d6dbda5c
Temitayo Adejare <temitayoadejare@icloud.com>
2023-08-01 16:15:07 -0500

    Initial Commit

---
+++ Clarity/ClarityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClarityApp.swift
+//  Clarity
+//
+//  Created by Temitayo Adejare on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ClarityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
